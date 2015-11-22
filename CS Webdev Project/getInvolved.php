bfd8632c6d0ecafb4675dea87039e10a4a8176f4
MrGamer <JCarpenter11@winona.edu>
2015-11-22 17:37:39 -0600

    Basic Additions, page shortcuts in involved

--- CS Webdev Project/getInvolved.php
+++ CS Webdev Project/getInvolved.php
@@ -14,17 +14,32 @@
 	?>
 	
 	<div id="sidenav">
-		Side Navigational Content
+		<a href="#Time">Donate Time</a>
 		<br>
-		<a href="" title="">Quicklink</a>
+		<a href="#Money">Donate Money</a>
+		<br>
+		<a href="#Items">Donate Items</a>
 		<br>
 	</div>
 
 	<div id="maincontent">
 		<div class="text">
-			
+			<h1>Kids First Accepts all sorts of donations!</h1>
+			Dontate your time, some spare cash, or some gently used items.
 		</div>
-
+		<div class="text" id="Time">
+			<h1>Kids First Accepts all sorts of donations!</h1>
+			Dontate your time, some spare cash, or some gently used items.
+		</div>
+		<div class="text" id="Money">
+			<h1>Kids First Accepts all sorts of donations!</h1>
+			Dontate your time, some spare cash, or some gently used items.
+		</div>
+		<div class="text" id="Items">
+			<h1>Donating Items</h1>
+			Kids first gets by on generous donations of everything from older computers to chairs
+			, couches, and cabinets.
+		</div>
 	</div>
 
 
