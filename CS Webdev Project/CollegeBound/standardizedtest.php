128fe9b307aa14234709152439f3de723c03564c
Evilstrick <christopher.bm.rogers@gmail.com>
2015-12-02 14:34:00 -0600

    ACT And SAT stuff

--- CS Webdev Project/CollegeBound/standardizedtest.php
+++ CS Webdev Project/CollegeBound/standardizedtest.php
@@ -8,7 +8,10 @@
 </head>
 
 <body>
-	<?php include 'header.html' ?>
+	<?php
+	$folders = 1; 
+	include "../header.php"; 
+	?>
 	<div id="sidenav">
 		Side Navigational Content
 		<br>
@@ -17,17 +20,36 @@
 	</div>
 	<div id="maincontent">
 		<div class="text">
-			<h1>Welcome to the Kids First College Bound program!</h1>
-			<br>
-			<p>The transition from high school is an exciting time, and there are many questions you have to ask yourself.</p>
-			<p>What do you want to do with your life?  What do you want to become?</p>
-			<br>
-			<p>If your goals for the future require a higher education, then it can be difficult to know where to begin.  We're here to help make the process understandable.</p>
-			<a href="choosingaschool">Follow Me!</a>
+			<h1>Standardized Tests!</h1>
+			<p>Standardized tests are an import part of applying to college. These test are used by schools to determine: admittance, scholarships, 
+				and starting classes. Colleges put a heavy importance on these test and as such, so should you. The two main standardized 
+				tests are the SAT and ACT. The SAT is used mainly on the East Coast and West Coast and by highly selective colleges and universities.
+				 The ACT is used mainly in the in Midwest. For example, the University of Maryland uses the SAT on the application while 
+				Winona State University uses the ACT. So, depending on which school you want to apply is the determining factor on which 
+				test you should take. There is nothing wrong with taking both tests to keep options open! </p>
 		</div>
-
-		<div class="text">
-			Main Content is present
+		<div class = "text">
+			<h2>The ACT</h2>
+			<p>The ACT is most likely going to be the standardized test that you will be taking. The test assesses four areas of skill: English, Reading,
+			 Mathematics, Science, and an optional Writing section. The English section will measure your ability to comprehend written English and your
+			 rhetorical skills. The Reading section is just a simple measure of how well you can read and understand information presented to you in a 
+			given amount of time. The Mathematics section will test your ability to perform mathematical calculations learned in class up to the
+			 beginning of twelfth grade. All the math you have taken up to grade twelve will be on the test, but do not worry, most of it is just
+			 algebra. The Science section measures the interpretation, analysis, evaluation, reasoning, and problem-solving skills required in the 
+			natural sciences. The Writing section measures writing skills emphasized in high school English classes and in entry-level college 
+			composition courses.</p>
+		</div>
+		<div class = "text">
+			<h2>The SAT</h2>
+			<p>The SAT is a three parts that test the skill you have learned in high school. The SAT tests your skill in Critical Reading, Writing,
+			 and Mathematics. The Critical Reading section includes reading passages and understanding the information given and completing sentences to
+			 be grammatically correct. The Writing section includes a short essay prompt and several multiple choice questions relating to identifying
+			 errors and improving grammar and usage. The Mathematics section includes questions on arithmetic operation, algebra, geometry, statistics
+			 and probability.</p>
+		</div>
+		<div>
+			<h2>Quick Compare</h2>
+			
 		</div>
 
 	</div>
