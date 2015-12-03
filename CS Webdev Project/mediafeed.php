e85939795355ce18c473a7b6489bfef9310c834c
Evilstrick <christopher.bm.rogers@gmail.com>
2015-12-03 16:42:06 -0600

    Media Feed Done

--- CS Webdev Project/mediafeed.php
+++ CS Webdev Project/mediafeed.php
@@ -1,3 +1,4 @@
-<div id="feed">
-Side Feed
+<div id = "mediafeed">
+<a class="twitter-timeline"  href="https://twitter.com/MrRogersishere" data-widget-id="672535477958762496">Tweets by @MrRogersishere</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
