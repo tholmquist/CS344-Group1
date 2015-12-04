112b8941e7be78d788c813da38c16ce4153da121
jmallian <jlmallian@gmail.com>
2015-12-04 13:12:37 -0600

    includes a map now

--- CS Webdev Project/index.php
+++ CS Webdev Project/index.php
@@ -9,7 +9,7 @@
 	<link href="style.css" type="text/css" rel="stylesheet">
 </head>
 
-<body>
+<body class="houseLink">
 	<?php
 		$folders = 0;
 		include 'header.php';
@@ -17,36 +17,35 @@
 	?>
 	
 	<div id="sidenav">
-		Images/Gallery
-		<br>
+		<iframe
+		frameborder="0" style="border:0"
+		src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDImZu0jrgeHP_CgxMtC6j9UGiYH9unWSs&q=1756+Kraemer+Dr,+Winona,+MN+55987">
+		</iframe>
 	</div>
 
 	<div id="maincontent">
-		<img src="Pictures/IMG_7210.jpg">
-		<div class="text">
-			Kids First is as afterschool program serving Maplewood residents who are in kindergarten through 12th 
+			<p>Kids First is as after school program serving Maplewood residents who are in kindergarten through 12th 
+			grade. It is staffed by WSU student volunteers and provides assistance with homework, tutoring, and 
+			recreational activities. We also provide snacks. Kids First has been helping the community for the last 8 
+			years and currently there are about 100 kids in the program, with a daily attendance of around 50 kids. 
+			Our goal is to prepare Maplewood residents for university and long term success in life.</p>
 
-grade. It is staffed by WSU student volunteers and provides assistance with homework, tutoring, and 
+			<p>Kids first follows Winona State University schedule however we do close if the <a href="http://www.winona.k12.mn.us/" target="_blank">local school district</a> 
+			calls a snow day. Our Fall term is Tuesday, September 1st – Thursday, December 3rd 2015.</p>
+			
+			<p>Kids First will be closed on Labor Day (09/07), <br>
+			Veterans Day (11/11), <br>
+			& Thanksgiving (11/25 & 11/26). <br>
+			Otherwise we are opened Monday – Thursday, 
+			2:30 – 6:30 p.m.</p>
+			
+			<p>We are located at: <br>
+			1756 Kraemer Dr. <br>
+			Winona, MN 55987<p>
+	</div>
 
-recreational activities. We also provide snacks. Kids First has been helping the community for the last 8 
-
-years and currently there are about 100 kids in the program, with a daily attendance of around 50 kids. 
-
-Our goal is to prepare Maplewood residents for university and long term success in life. 
-
-Kids first follows Winona State University schedule however we do close if the local school district calls a 
-
-snow day [this http://www.winona.k12.mn.us/ site will put up a notice if it’s a snow day]. Our Fall term 
-
-is Tuesday, September 1st – Thursday, December 3rd 2015. Kids First will be closed on Labor Day (09/07), 
-
-Veterans Day (11/11), & Thanksgiving (11/25 & 11/26). Otherwise we are opened Monday – Thursday, 
-
-2:30 – 6:30 p.m.
-
-		</div>
-		<img src="Pictures/IMG_7209.jpg">
-
+	<div id="mediafeed">
+		<img src="Pictures/IMG_7210.jpg" alt="kids first" title="kids first">
 	</div>
 
 
