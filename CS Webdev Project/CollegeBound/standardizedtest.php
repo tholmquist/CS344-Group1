f3989eeffe30a1dda8c9aa7da1302b5f7a332032
Evilstrick <christopher.bm.rogers@gmail.com>
2015-12-04 13:22:35 -0600

    Changed somethings

--- CS Webdev Project/CollegeBound/standardizedtest.php
+++ CS Webdev Project/CollegeBound/standardizedtest.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<?php $folders = 1;?>
+
 <head>
 	<meta charset="utf-8">
 	<title>Kids First</title>
@@ -22,22 +22,22 @@
 		<div class="text">
 			<h1>Standardized Tests!</h1>
 			<p>Standardized tests are an import part of applying to college. These test are used by schools to determine: admittance, scholarships, 
-				and starting classes. Colleges put a heavy importance on these test and as such, so should you. The two main standardized 
-				tests are the SAT and ACT. The SAT is used mainly on the East Coast and West Coast and by highly selective colleges and universities.
-				 The ACT is used mainly in the in Midwest. For example, the University of Maryland uses the SAT on the application while 
-				Winona State University uses the ACT. So, depending on which school you want to apply is the determining factor on which 
-				test you should take. There is nothing wrong with taking both tests to keep options open! </p>
+			and starting classes. Colleges put a heavy importance on these test and as such, so should you. The two main standardized 
+			tests are the SAT and ACT. The SAT is used mainly on the East Coast and West Coast and by highly selective colleges and universities. 
+			The ACT is used mainly in the in Midwest. For example, the University of Maryland uses the SAT on the application while 
+			Winona State University uses the ACT. So, depending on which school you want to apply is the determining factor on which 
+			test you should take. There is nothing wrong with taking both tests to keep options open!</p>
 		</div>
 		<div class = "text">
 			<h2>The ACT</h2>
 			<p>The ACT is most likely going to be the standardized test that you will be taking. The test assesses four areas of skill: English, Reading,
 			 Mathematics, Science, and an optional Writing section. The English section will measure your ability to comprehend written English and your
 			 rhetorical skills. The Reading section is just a simple measure of how well you can read and understand information presented to you in a 
-			given amount of time. The Mathematics section will test your ability to perform mathematical calculations learned in class up to the
-			 beginning of twelfth grade. All the math you have taken up to grade twelve will be on the test, but do not worry, most of it is just
+			 given amount of time. The Mathematics section will test your ability to perform mathematical calculations learned in class up to the
+			 beginning of twelfth grade.</p><p>All the math you have taken up to grade twelve will be on the test, but do not worry, most of it is just
 			 algebra. The Science section measures the interpretation, analysis, evaluation, reasoning, and problem-solving skills required in the 
-			natural sciences. The Writing section measures writing skills emphasized in high school English classes and in entry-level college 
-			composition courses.</p>
+			 natural sciences. The Writing section measures writing skills emphasized in high school English classes and in entry-level college 
+			 composition courses.</p>
 		</div>
 		<div class = "text">
 			<h2>The SAT</h2>
@@ -49,12 +49,24 @@
 		</div>
 		<div>
 			<h2>Quick Compare</h2>
-			
+			<h3 id = "ACT">THE ACT</h3>
+			<h3 id = "SAT">THE SAT</h3>
+			<img alt="Quick Compare ACT and SAT" src = "../Assets/VennDiagram.png">
 		</div>
-
+		<div class = "text">
+			<h2>Preparing For The Test</h2>
+			<p>Once you have come to the decision on what test is appropriate for you, and it may be both, it is time to register to take the test. 
+			Before you continue with the registration you should make sure that you are well prepared for the test. Studying for these test is 
+			not like studying for your standard English, Math, or Science test. Studying for these test is more like studying for the test itself, not 
+			the content on the test.  Look to see if your school has any classes or ACT or SAT preparation, those are very helpful and taking the class 
+			will prepare you much better than preparing on your own. 
+		</div>
+		<div class ="text">
+			<h2>Registering For The Test</h2>
+			<p>Register for the ACT <a href = "http://www.actstudent.org/regist/">here</a>.</p>
+			<p>Register for the SAT <a href = "https://sat.collegeboard.org/register">here</a>.</p>
+		</div>
 	</div>
-
-
 </body>
 
 </html>
